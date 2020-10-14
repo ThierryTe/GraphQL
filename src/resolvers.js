fa3de53c5c2b43a37700957622eea280a68d2e1d
KIMA Tewende <tewende.kima@nebrata.com>
2020-10-14 13:21:51 +0000

    Utilisation de graphQL avec node js

---
+++ src/resolvers.js
@@ -0,0 +1,37 @@
+let users = [
+  { id: 1, nom: "Thierry", email: "thierry@gmail.com", age: 17 },
+  { id: 2, nom: "Tewendé", email: "tewende@gmail.com", age: 37 }
+]
+
+
+const messageHello = "Hello !"
+const resolvers = {
+  Query: {
+    hello: (parent, args, content, info) => messageHello,
+    users: () => users,
+    user: (parent, args) => users.find(user => user.id == args.id)
+  },
+  Mutation: {
+    createUser: (parent, { id, nom, email, age }) => {
+      let checkID = users.findIndex(user => user.id == id)
+      if (checkID == -1) {
+        let newUser = { id, nom, email, age }
+        users.push(newUser)
+        return newUser
+      } else {
+        throw new Error('cet id existe déjà')
+      }
+    },
+    deleteUser: (parent, { id }) => {
+      let checkID = users.findIndex(user => user.id == id)
+      if (checkID != -1) {
+        users.splice(checkID, 1)
+        return true
+      } else {
+        throw new Error('Utilisateur abscent')
+      }
+    }
+
+  }
+}
+export default resolvers
